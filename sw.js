3657b48b9eafd83f16edb59bb8de85a1bceaf7ea
nbrfr55557-jpg <pierrick.stm@gmail.com>
2026-03-19 12:02:59 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'checklist-tigre-v1';
-const FILES = ['/CHECKLIST/CHECKLIST.html'];
+const CACHE = 'checklist-tigre-v2';
+const FILES = ['/CHECKLIST/index.html'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
